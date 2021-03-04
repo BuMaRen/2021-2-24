0dd9b64a2df786d76e1fd48059c4d5706676014f
Desktop_MaSha <Masha@DESKTOP-UN3AAGC>
2021-03-05 00:50:34 +0800

    2021/3/5 0:50 get kmp next array

---
+++ component/functions/kmp.c
@@ -0,0 +1,49 @@
+#include "functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static Separator_t single_separator = SEPARATOR_NULL;
+
+// ready for kmp
+static int *getNextArray(char *separator)
+{
+    if (separator == NULL || *separator == '\0') return NULL;
+    char *ptr = separator + 1;
+    
+    if (*ptr == '\0')
+    {
+        single_separator = SEPARATOR_SINGLE;
+        return NULL;
+    }
+    
+    int size = strlen(separator);
+    int *next = (int *)malloc(sizeof(int)*(size + 1));
+    
+    if (next == NULL)
+    {
+        perror("malloc next array failed!");
+        return NULL;
+    }
+    
+    next[0] = -1;
+    next[1] = 0;
+    int temp = 0;
+    unsigned int i = 1;
+    for (; i < size; ++i)
+    {
+        temp = next[i];
+        while (temp != -1)
+        {
+            if (separator[i] == separator[temp])
+            {
+                next[i+1] = next[i] + 1;
+                break;
+            }
+            else temp = next[temp];
+        }
+        if (temp == -1) next[i+1] = 0;
+    }
+    return next;
+}
+
--- component/include/mashatypes.h
+++ component/include/mashatypes.h
@@ -1,4 +1,12 @@
+
+// type for Separator_t
+#define SEPARATOR_NULL      0
+#define SEPARATOR_SINGLE    1
+#define SEPARATOR_MUTI      2
+
 typedef int Socketfd_t;
 typedef uint16_t Port_t;
 typedef unsigned int BufferSize_t;
 typedef enum {False = 0, True = 1}Bool;
+
+typedef unsigned char Separator_t;
